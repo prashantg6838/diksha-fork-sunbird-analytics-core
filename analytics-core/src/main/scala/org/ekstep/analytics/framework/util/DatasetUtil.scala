6195b9142f71c268eb20143bff72d439b6ed3648
Shubham Sharma <shubham.sharma@trigyn.com>
2023-11-13 19:14:46 +0530

    revert code

--- analytics-core/src/main/scala/org/ekstep/analytics/framework/util/DatasetUtil.scala
+++ analytics-core/src/main/scala/org/ekstep/analytics/framework/util/DatasetUtil.scala
@@ -2,11 +2,10 @@ package org.ekstep.analytics.framework.util
 
 import net.lingala.zip4j.ZipFile
 import org.apache.spark.sql.functions.col
-import org.apache.spark.sql.{Dataset, Row, SparkSession}
+import org.apache.spark.sql.{Dataset, Row}
 import org.ekstep.analytics.framework.StorageConfig
 import org.sunbird.cloud.storage.BaseStorageService
 import org.apache.hadoop.conf.Configuration
-import org.apache.spark.util.SizeEstimator
 import org.ekstep.analytics.framework.conf.AppConf
 
 import java.io.File
@@ -18,10 +17,6 @@ class DatasetExt(df: Dataset[Row]) {
 
   private def getTempDir(filePrefix: String, reportId: String): String = {
     Paths.get(filePrefix, reportId, "/_tmp/").toString()
-  }
-
-  private def getTempDir2(filePrefix: String, reportId: String): String = {
-    Paths.get(filePrefix, reportId, "/_tmp1/").toString()
   }
 
   private def getFinalDir(filePrefix: String, reportId: String): String = {
@@ -42,11 +37,8 @@ class DatasetExt(df: Dataset[Row]) {
   }
 
   def saveToBlobStore(storageConfig: StorageConfig, format: String, reportId: String, options: Option[Map[String, String]],
-                      partitioningColumns: Option[Seq[String]],
-                      storageService: Option[BaseStorageService] = None,
-                      zip: Option[Boolean] = Option(false),
-                      columnOrder: Option[List[String]] = Option(List()),
-                      fileExt:Option[String] = None): List[String] = {
+                      partitioningColumns: Option[Seq[String]], storageService: Option[BaseStorageService] = None,
+                      zip: Option[Boolean] = Option(false), columnOrder: Option[List[String]] = Option(List()), fileExt:Option[String] = None): List[String] = {
 
     val conf = df.sparkSession.sparkContext.hadoopConfiguration;
 
@@ -78,7 +70,6 @@ class DatasetExt(df: Dataset[Row]) {
 
     val tempDir = getTempDir(file, reportId);
     val finalDir = getFinalDir(file, reportId);
-    val tempDir2 = getTempDir2(file, reportId)
 
     val dims = partitioningColumns.getOrElse(Seq())
     var headersList = columnOrder.getOrElse(List())
@@ -96,30 +87,7 @@ class DatasetExt(df: Dataset[Row]) {
         df.repartition(1).select(headersList.head, headersList.tail: _*).write.format(format).options(opts).save(filePrefix + tempDir)
       }
       else {
-       /* println(s"fileprefix = $filePrefix  tempDir=$tempDir format=$format")
-        println(opts.mapValues(_.toList))
-        opts.foreach { case (key, values) => println("key " + key + " - " + values.mkString("-"))}
-        println(df.printSchema())
-        println(df.show(10)) */
-       val dfSize = SizeEstimator.estimate(df)
-        println(s"Estimated size of the dataFrame someDF = ${dfSize/1000000} mb $dfSize")
-        println(s"TempDir ==  $tempDir2")
-        df.printSchema()
-        df.show(5)
-
-        df.repartition(4).write.format(format).options(opts).save(filePrefix + tempDir2)
-
-        val partitionDf = df.sparkSession.read.option("header", true).csv(filePrefix + tempDir2)
-
-         println(s"spark read the data from path = ${filePrefix + tempDir2}")
-
-        partitionDf.show()
-
-        partitionDf.repartition(1)
-            .write.format(format)
-            .options(opts)
-            .save(filePrefix + tempDir)
-        //df.coalesce(1).write.format(format).options(opts).save(filePrefix + tempDir)
+        df.repartition(1).write.format(format).options(opts).save(filePrefix + tempDir)
       }
       fileUtil.delete(conf, filePrefix + finalDir + "." + fileExt.getOrElse(format))
       fileUtil.copyMerge(filePrefix + tempDir, filePrefix + finalDir + "." + fileExt.getOrElse(format), conf, true);
